1125c770b63753581292a8f36a58bf4c0d7b7c18
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-08 14:12:14 +0800

    change database username

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 // used to connect to the database
-$host = "localhost";
+$host = "162.241.217.198";
 $db_name = "megawor3_EBsystem";
 $username = "megawor3_appsys";
 $password = "Dr8amb15!";
