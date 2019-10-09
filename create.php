3a71df3358eeff642f10dc10434a67c9e111275e
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-09 15:16:33 +0800

    v0.2

--- create.php
+++ create.php
@@ -16,19 +16,7 @@
 
 </head>
 <body>
-<script>
-function GetSelectedValue(){
-    var e = document.getElementById("unittype");
-    var result = e.options[e.selectedIndex].value;
-                        
-    var x = document.getElementById("city")
-    var resultx = x.options[x.selectedIndex].value;
-
-    var final = result + resultx  
-
-    document.getElementById("result").innerHTML = final;
-}
-</script>        
+     
 
 
     <!-- container -->
@@ -517,6 +505,20 @@ function GetSelectedValue(){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="form-validation.js"></script>
+
+    <script>
+function GetSelectedValue(){
+    var e = document.getElementById("unittype");
+    var result = e.options[e.selectedIndex].value;
+                        
+    var x = document.getElementById("city")
+    var resultx = x.options[x.selectedIndex].value;
+
+    var final = result + resultx  
+
+    document.getElementById("result").innerHTML = final;
+}
+</script>   
 
 </body>
 </html>
