82af2f47750ed246493a6fd5381ea340ce2b8585
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-09 15:25:38 +0800

    v0.4

--- create.php
+++ create.php
@@ -10,6 +10,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="form-validation.css">
+    <script src="form-validation.js"></script>
 
 
 
@@ -501,10 +502,10 @@
     
  <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="form-validation.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="form-validation.js"></script>
 
     <script>
 function GetSelectedValue(){
@@ -514,9 +515,9 @@ function GetSelectedValue(){
     var x = document.getElementById("city")
     var resultx = x.options[x.selectedIndex].value;
 
-    var final = result + resultx  
+    var unitcode = result + resultx  
 
-    document.getElementById("result").innerHTML = final;
+    document.getElementById("result").innerHTML = unitcode;
 }
 </script>   
 
