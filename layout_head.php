d2738c4cbfd7428201ae9babf29e8cf7f033a76a
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-15 11:23:52 +0800

    v6

--- layout_head.php
+++ layout_head.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
  
     <!-- admin custom CSS -->
-    <link href="<?php echo $home_url . "https://www.everright.com.ph/everbrightapp/libs/css/customer.css" ?>" rel="stylesheet" />
+    <link href="<?php echo $home_url . "https://www.everright.com.ph/everbrightapp/libs/css/login.css" ?>" rel="stylesheet" />
  
 </head>
 <body>
