1b479186054bbbf8b3fdd0f0fb595127a7825729
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-15 11:22:45 +0800

    v5

--- login.php
+++ login.php
@@ -24,20 +24,33 @@ echo "<div class='col-sm-6 col-md-4 col-md-offset-4'>";
     // alert messages will be here
  
     // actual HTML login form
-    echo "<div class='account-wall'>";
-        echo "<div id='my-tab-content' class='tab-content'>";
-            echo "<div class='tab-pane active' id='login'>";
-                echo "<img class='profile-img' src='images/login-icon.png'>";
-                echo "<form class='form-signin' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post'>";
-                    echo "<input type='text' name='email' class='form-control' placeholder='Email' required autofocus />";
-                    echo "<input type='password' name='password' class='form-control' placeholder='Password' required />";
-                    echo "<input type='submit' class='btn btn-lg btn-primary btn-block' value='Log In' />";
-                echo "</form>";
-            echo "</div>";
-        echo "</div>";
-    echo "</div>";
  
 echo "</div>";
+
+echo "<form class='form-signin' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post'>";
+echo " <div class='text-center mb-4'>";
+echo "   <img class='mb-4' src='{{ site.baseurl }}/docs/{{ site.docs_version }}/assets/brand/bootstrap-solid.svg' alt='' width='72' height='72'>";
+echo "    <h1 class='h3 mb-3 font-weight-normal'>Floating labels</h1>";
+echo "   <p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element. <a href='https://caniuse.com/#feat=css-placeholder-shown'>Works in latest Chrome, Safari, and Firefox.</a></p>";
+    echo " </div>";
+
+    echo "  <div class='form-label-group'>";
+    echo " <input type='email' id='inputEmail' class='form-control' placeholder='Email address' required autofocus>";
+    echo "  <label for='inputEmail'>Email address</label>";
+    echo " </div>";
+
+    echo " <div class='form-label-group'>";
+    echo " <input type='password' id='inputPassword' class='form-control' placeholder='Password' required>";
+    echo "  <label for='inputPassword'>Password</label>";
+    echo " </div>";
+
+  echo " <div class='checkbox mb-3'>";
+  echo "   <label>";
+    echo "     <input type='checkbox' value='remember-me'> Remember me </label>";
+    echo " </div>";
+  echo " <button class='btn btn-lg btn-primary btn-block' type='submit'>Sign in</button>";
+  echo " <p class='mt-5 mb-3 text-muted text-center'>&copy; 2017-{{ site.time | date: '%Y' }}</p>";
+  echo "</form>";
  
 // footer HTML and JavaScript codes
 include_once "layout_foot.php";
