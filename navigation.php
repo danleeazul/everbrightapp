bcb35cb27445cca21162a6db654011d4d391faf8
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-17 16:46:32 +0800

    v20.10

--- navigation.php
+++ navigation.php
@@ -8,25 +8,23 @@
       <li class="nav-item active">
         <a class="nav-link" href="<?php echo $home_url; ?>">Home <span class="sr-only">(current)</span></a>
       </li>
-    </ul>
     <?php
             // login and logout options will be here 
             // check if users / customer was logged in
             // if user was logged in, show "Edit Profile", "Orders" and "Logout" options
             if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']==true && $_SESSION['access_level']=='Customer'){
                 ?>
-                <ul class="nav navbar-nav navbar-right">
-                    <li <?php echo $page_title=="Edit Profile" ? "class='active'" : ""; ?>>
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                            &nbsp;&nbsp;<?php echo $_SESSION['firstname']; ?>
-                            &nbsp;&nbsp;<span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo $home_url; ?>logout.php">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                
+                     <li class="nav-item">  
+                           <?php echo $_SESSION['firstname']; ?>
+                     </li>   
+
+                     <li class="nav-item">
+                                <a href="<?php echo $home_url; ?>logout.php">Logout</a>
+                     </li>
+                        
+                    
+                
                 <?php
                 }
                 
@@ -34,19 +32,19 @@
                 // if user was not logged in, show the "login" and "register" options
                 else{
                     ?>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li <?php echo $page_title=="Login" ? "class='active'" : ""; ?>>
+                    
+                    <li class="nav-item">
                             <a href="<?php echo $home_url; ?>login">
                                 <span class="glyphicon glyphicon-log-in"></span> Log In
                             </a>
                         </li>
                     
-                        <li <?php echo $page_title=="Register" ? "class='active'" : ""; ?>>
+                        <li class="nav-item">
                             <a href="<?php echo $home_url; ?>register">
                                 <span class="glyphicon glyphicon-check"></span> Register
                             </a>
                         </li>
-                    </ul>
+                
                     <?php
                     }
             ?>
