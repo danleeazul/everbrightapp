62d734ee81c262acec82957c77262ba36ad09165
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-17 16:51:22 +0800

    v20.11

--- config/database.php
+++ config/database.php
@@ -1,16 +1,26 @@
 <?php
-// used to connect to the database
-$host = "localhost";
-$db_name = "megawor3_EBsystem";
-$username = "megawor3_appsys";
-$password = "everbright1688";
-  
-try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-}
-  
-// show error
-catch(PDOException $exception){
-    echo "Connection error: " . $exception->getMessage();
+// used to get mysql database connection
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "megawor3_EBsystem";
+    private $username = "megawor3_appsys";
+    private $password = "everbright1688";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
 }
 ?>
