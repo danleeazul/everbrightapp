904037dce10a68202016a92872793d4dc4357d8b
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-24 14:20:27 +0800

    v3

--- login.php
+++ login.php
@@ -102,7 +102,6 @@ if($access_denied){
     echo "<div class='account-wall'>";
         echo "<div id='my-tab-content' class='tab-content'>";
             echo "<div class='tab-pane active' id='login'>";
-                echo "<img class='profile-img' src='images/login-icon.png'>";
                 echo "<form class='form-signin' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post'>";
                     echo "<input type='text' name='email' class='form-control' placeholder='Email' required autofocus />";
                     echo "<input type='password' name='password' class='form-control' placeholder='Password' required />";
