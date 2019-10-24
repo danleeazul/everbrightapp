cb60457ed956163ec1f671ae090e8865da128d7c
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-24 14:53:02 +0800

    v8

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
 <?php
 // core configuration
 include_once "config/core.php";
-include_once "config/database.php";
-include_once "objects/user.php";
+
 
 // set page title
 $page_title = "Login";
@@ -13,8 +12,55 @@ include_once "login_checker.php";
  
 // default to false
 $access_denied=false;
-
-
+echo "SAMPLE";
+// post code will be here
+// if the login form was submitted
+if($_POST){
+    // email check will be here
+    // include classes
+include_once "config/database.php";
+include_once "objects/user.php";
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// initialize objects
+$user = new User($db);
+ 
+// check if email and password are in the database
+$user->email=$_POST['email'];
+// check if email exists, also get user details using this emailExists() method
+$email_exists = $user->emailExists();
+ 
+// login validation will be here
+// validate login
+if ($email_exists && password_verify($_POST['password'], $user->password) && $user->status==1){
+ 
+    // if it is, set the session value to true
+    $_SESSION['logged_in'] = true;
+    $_SESSION['user_id'] = $user->id;
+    $_SESSION['access_level'] = $user->access_level;
+    $_SESSION['firstname'] = htmlspecialchars($user->firstname, ENT_QUOTES, 'UTF-8') ;
+    $_SESSION['lastname'] = $user->lastname;
+ 
+    // if access level is 'Admin', redirect to admin section
+    if($user->access_level=='Admin'){
+        header("Location: {$home_url}admin/index.php?action=login_success");
+    }
+ 
+    // else, redirect only to 'Customer' section
+    else{
+        header("Location: {$home_url}index.php?action=login_success");
+    }
+}
+ 
+// if username does not exist or password is wrong
+else{
+    $access_denied=true;
+}
+}
+ 
 // login form html will be here
 // include page header HTML
 include_once "layout_head.php";
@@ -51,75 +97,24 @@ if($access_denied){
         Your username or password maybe incorrect
     </div>";
 }
- 
-    // actual HTML login form
-    echo "<div class='account-wall'>";
-        echo "<div id='my-tab-content' class='tab-content'>";
-            echo "<div class='tab-pane active' id='login'>";
-                echo "<form class='form-signin' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post'>";
-                    echo "<input type='text' name='email' class='form-control' placeholder='Email' required autofocus />";
-                    echo "<input type='password' name='password' class='form-control' placeholder='Password' required />";
-                    echo "<input type='submit' class='btn btn-lg btn-primary btn-block' value='Log In' />";
-                echo "</form>";
-            echo "</div>";
-        echo "</div>";
-    echo "</div>";
- 
 echo "</div>";
-
-
-
+?>
  
-// post code will be here
-// if the login form was submitted
-if($_POST){
-    // email check will be here
-    // include classes
-
- 
-// get database connection
-$database = new Database();
-$db = $database->getConnection();
- 
-// initialize objects
-$user = new User($db);
- 
-// check if email and password are in the database
-$user->email=$_POST['email'];
-echo "User:".$user;
-// check if email exists, also get user details using this emailExists() method
-$email_exists = $user->emailExists();
- 
-// login validation will be here
-// validate login
-if ($email_exists && password_verify($_POST['password'], $user->password) && $user->status==1){
- 
-    // if it is, set the session value to true
-    $_SESSION['logged_in'] = true;
-    $_SESSION['user_id'] = $user->id;
-    $_SESSION['access_level'] = $user->access_level;
-    $_SESSION['firstname'] = htmlspecialchars($user->firstname, ENT_QUOTES, 'UTF-8') ;
-    $_SESSION['lastname'] = $user->lastname;
- 
-    // if access level is 'Admin', redirect to admin section
-    if($user->access_level=='Admin'){
-        header("Location: {$home_url}admin/index.php?action=login_success");
-    }
- 
-    // else, redirect only to 'Customer' section
-    else{
-        header("Location: {$home_url}index.php?action=login_success");
-    }
-}
- 
-// if username does not exist or password is wrong
-else{
-    $access_denied=true;
-}
-}
+    
+    <div class='account-wall'>
+        <div id='my-tab-content' class='tab-content'>
+           <div class='tab-pane active' id='login'>
+           <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                    <input type='text' name='email' class='form-control' placeholder='Email' required autofocus />
+                    <input type='password' name='password' class='form-control' placeholder='Password' required />
+                   <input type='submit' class='btn btn-lg btn-primary btn-block' value='Log In' />
+               </form>
+            </div>
+       </div>
+    </div>
  
 
- 
+<?php 
 // footer HTML and JavaScript codes
 include_once "layout_foot.php";
 ?>
