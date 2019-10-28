71afd399a3a09e667089840b8d1a2c99ed0d43b2
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-28 15:07:31 +0800

    v14.7

--- indexsample.php
+++ indexsample.php
@@ -90,7 +90,7 @@ echo "<div class='mdc-drawer-app-content'>";
         echo "                  <br />";
         echo "                  <small class='text-muted'>{$date}</small>";
         echo "                        </td>";
-        echo "                        <td style='width: 100px;'>";
+        echo "                        <td style='width: 400px;'>";
             
         echo "                <span class='text-muted'>₱{$price}</span>";
         echo "                        </td>";
