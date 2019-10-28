fe010f1dd2b9cbe53cc8c4a3629453e51899a3e6
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-28 15:49:24 +0800

    v14.10

--- create_deals.php
+++ create_deals.php
@@ -149,25 +149,6 @@ if($_POST){
     <script>
  function GetSelectedValue(){
 
-(function () {
-'use strict'
-
-window.addEventListener('load', function () {
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.getElementsByClassName('needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.filter.call(forms, function (form) {
-    form.addEventListener('submit', function (event) {
-      if (form.checkValidity() === false) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-      form.classList.add('was-validated')
-    }, false)
-  })
-}, false)
-}())
 
   var e = document.getElementById("name");
   var result = e.options[e.selectedIndex].value;
