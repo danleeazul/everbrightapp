5f542378c851e7c63cff439c193c7dc4d6469eb2
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-28 15:47:24 +0800

    v14.9

--- create_deals.php
+++ create_deals.php
@@ -34,7 +34,7 @@ if($_POST){
     try{
      
         // insert query
-        $query = "INSERT INTO tbl_deals SET name=:name, building=:building, unit_no=:unit_no, type=:type, price=:price";
+        $query = "INSERT INTO tbl_deals SET name=:name, building=:building, unit_no=:unit_no, type=:type, price=:price, date=:date";
  
         // prepare query for execution
         $stmt = $con->prepare($query);
@@ -45,6 +45,7 @@ if($_POST){
         $unit_no=htmlspecialchars(strip_tags($_POST['unit_no']));
         $type=htmlspecialchars(strip_tags($_POST['type']));
         $price=htmlspecialchars(strip_tags($_POST['price']));
+        $date=htmlspecialchars(strip_tags($_POST['date']));
 
  
         // bind the parameters
@@ -53,6 +54,10 @@ if($_POST){
         $stmt->bindParam(':unit_no', $unit_no);
         $stmt->bindParam(':type', $type);
         $stmt->bindParam(':price', $price);
+
+        // specify when this record was inserted to the database
+        $created=date('Y-m-d H:i:s');
+        $stmt->bindParam(':created', $created);
          
          
         // Execute the query
@@ -77,9 +82,17 @@ if($_POST){
       <hr class="mb-4">
        <div class="row">
                <div class="col-md-4 mb-3">
-                   <label for="firstName">Name</label>
-                   <input type="text" class="form-control" name="name" placeholder="" value="" required>  
-                               
+               <label for="lastName">Name</label>
+                    <select class="custom-select d-block w-100" name="name" onchange="GetSelectedValue()" required>
+                      <option value="">Select...</option>
+                      <option value="https://www.everbright.com.ph/headshot/EB-Joana.png">Joana Marie Legaspo</option>
+                      <option value="https://www.everbright.com.ph/headshot/EB-Aj.png">Aira Joy Lim</option> 
+                      <option value="https://www.everbright.com.ph/headshot/EB-Nica.png">Nica Ginez</option>
+                      <option value="https://www.everbright.com.ph/headshot/EB-Demi.png">Demi Dela Cruz</option>
+                      <option value="https://www.everbright.com.ph/headshot/EB-Bry.png">Bryan Sam Asis</option>
+                      <option value="https://www.everbright.com.ph/headshot/EB-Renz.png">Renz Ocampo</option>
+                    </select>           
+                    <p id="nameimage">as</p>     
                </div>
  
               <div class="col-md-4 mb-3">
@@ -133,6 +146,39 @@ if($_POST){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+    <script>
+ function GetSelectedValue(){
+
+(function () {
+'use strict'
+
+window.addEventListener('load', function () {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.getElementsByClassName('needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.filter.call(forms, function (form) {
+    form.addEventListener('submit', function (event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+}, false)
+}())
+
+  var e = document.getElementById("name");
+  var result = e.options[e.selectedIndex].value;
+
+  var unitcode = result
+
+
+
+ document.getElementById("nameimage").innerHTML = unitcode;
+}
+    </script>
   
 </body>
 </html>
