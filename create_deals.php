01a06a5eea245a6a397e47cb2be9fd349a1f5917
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-29 16:14:05 +0800

    v15

--- create_deals.php
+++ create_deals.php
@@ -57,7 +57,7 @@ if($_POST){
         $stmt->bindParam(':price', $price);
 
          // specify when this record was inserted to the database
-         $deals_date=date('Y-m-d',($deals_date));
+         $deals_date=date('Y-m-d H:i:s',($deals_date));
          $stmt->bindParam(':deals_date', $deals_date);
          
         // Execute the query
