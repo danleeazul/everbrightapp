fc7dbb8ec31a889ea4d9e1922024680606c97bea
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-30 11:37:42 +0800

    v5

--- create_req.php
+++ create_req.php
@@ -132,7 +132,7 @@ if($_POST){
              <input type="text" class="form-control" name="price" placeholder="Php" value="" required>     
            </div>
  
-          <div class="col-md-6 mb-3">
+          <div class="col-md-8 mb-3">
              <label for="firstName">Specification</label>
              <textarea class="form-control" id="exampleFormControlTextarea1" name="requirements" rows="3"></textarea>          
           </div>
