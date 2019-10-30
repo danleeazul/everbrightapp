ca35c999103cb322aebe260eacccd3c4ce0a4341
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-10-30 10:27:35 +0800

    v3

--- header.php
+++ header.php
@@ -8,10 +8,10 @@
     <title><?php echo isset($page_title) ? strip_tags($page_title) : "Everbright App"; ?></title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://www.everbright.com.ph/everbrightapp/libs/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> -->
+    <link rel="stylesheet" href="https://www.everbright.com.ph/everbrightapp/libs/css/style.css">
 
   </head>
   <body class="mdc-typography">
