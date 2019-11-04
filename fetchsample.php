6e0a1efe131313acb9d791111afb3a2220cb5d8d
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-04 16:49:42 +0800

    v8

---
+++ fetchsample.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'config/database.php';
+    $output = '';
+    $sql = "SELECT * FROM tbl_requirements WHERE requirements_id LIKE '%".$_POST["search"]."%'";
+    $result = mysqli_query($connect, $sql);
+ 
+    
+
+
+    if(mysqli_num_rows($result) > 0){
+
+        
+$output .='<h4>SEARCH</h4>';
+
+        while($row = mysqli_fetch_array($result)){
+
+                $output .='
+                <p>'.$row['building'].'</p>
+                                
+                ';
+            
+        }
+        echo $output;
+    }
+    else{
+        echo 'Data not found';
+    }
+?>
