d07b223a15d1556aa2ac1e51a36e5fc680b1693b
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-04 17:15:15 +0800

    v5

--- listing.php
+++ listing.php
@@ -226,7 +226,7 @@ echo "            </h4>";
 
 echo "            <ul class='list-group mb-3'>";
 
-$querya = "SELECT * FROM tbl_requirements WHERE requirements_id ='3' ";
+$querya = "SELECT * FROM tbl_requirements WHERE requirements_id LIKE '%3%' ";
 $stmta = $con->prepare($querya);
 $stmta->execute();
 
