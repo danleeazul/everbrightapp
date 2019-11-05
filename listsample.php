0d64a140abfb95dee636fae3c0d98bb26b172f0f
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-05 10:42:35 +0800

    v2

--- listsample.php
+++ listsample.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -250,32 +250,32 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     <script>
-       
-$(document).ready(function(){
-  load_data();
-  
-  function load_data(query)
- {
-    $.ajax({
-              url:"fetchsample.php",
-              method:"POST",
-              data:{query:query},
-              success:function(data)
-              {
-              $('#result').html(data);
-              }
-          });
- }
+    $(document).ready(function(){
 
- $('#search_text').keyup(function(){
-    var search = $(this).val();
-    if(search != ''){
-    load_data(search);
-    }
-    else{
-    load_data();
-    }
- });
+load_data();
+
+function load_data(query)
+{
+   $.ajax({
+             url:"fetchsample.php",
+             method:"POST",
+             data:{query:query},
+             success:function(data)
+             {
+             $('#result').html(data);
+             }
+         });
+}
+
+$('#search_text').keyup(function(){
+   var search = $(this).val();
+   if(search != ''){
+   load_data(search);
+   }
+   else{
+   load_data();
+   }
+});
 
 });
 
