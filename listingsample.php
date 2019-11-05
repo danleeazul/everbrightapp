6dfeb934dc65173efab3d9b6736e381f83e45b21
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-05 10:46:03 +0800

    v4

--- listingsample.php
+++ listingsample.php
@@ -16,7 +16,46 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
  </head>
- <body>
+ <body class="mdc-typography">
+
+ <aside  class='mdc-drawer'>
+                      <div id='mySidenav' class='sidenav'>
+                      <div>
+                    </div>
+                    <div style='height: 90%;' class='mdc-drawer__content'>
+                    
+                      <nav class='mdc-list'>
+                             
+
+                             <a id='navbutton' class='mdc-list-item'  href='indexsample.php' aria-selected='true'>
+                              <i class='material-icons mdc-list-item__graphic' aria-hidden='true'>dashboard</i>
+                          <span class='mdc-list-item__text'>Dashboard</span>
+                        </a>
+                        <a id='navbutton' class='mdc-list-item mdc-list-item--activated' >
+                          <i class='material-icons mdc-list-item__graphic' aria-hidden='true'>format_list_bulleted</i>
+                          <span class='mdc-list-item__text'>Listing</span>
+                        </a>
+                        <a id='navbutton' class='mdc-list-item' href='#' >
+                          <i class='material-icons mdc-list-item__graphic' aria-hidden='true'>person</i>
+                          <span class='mdc-list-item__text'>Accounts</span>
+                        </a>
+                        <hr class='mb-4'>                 
+                      </nav>
+
+                    </div>
+                    <div class='textbottom'> 
+          <table style='border: none; width: 100%;'>                       
+                      <tr>
+                        <td><p style='padding-left:20px;' class='text-muted'>© Everbright v0.0</p></td>
+                        <td style='padding-right:30px; padding-bottom:20px' class='text-right'><a href='logout.php' class='text-decoration-none'>Log out</a></td>
+                      </tr>   
+                  </table>
+        </div>
+                     </div>
+                         
+                  </aside>
+
+
   <div class="container">
    <br />
    <h2 >Ajax Live Data Search using Jquery PHP MySql</h2><br />
