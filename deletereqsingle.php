ce145cb90db898e28becce90dd5c1ed6454fc242
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-08 10:06:39 +0800

    v1

--- deletereqsingle.php
+++ deletereqsingle.php
@@ -7,10 +7,9 @@ try {
     $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
 
     // delete query
-    $query = "DELETE FROM tbl_requirements WHERE requirements_id = ?";
+    $query = "DELETE FROM tbl_requirementsWHERE id = ?";
     $stmt = $con->prepare($query);
-    $stmt->bindParam(1, $id);
-
+    
     if($stmt->execute()){
        // header('Location: index.php');
         echo "<script type='text/javascript'> document.location = 'https://www.everbright.com.ph/everbrightapp/indexsample.php'; </script>";
