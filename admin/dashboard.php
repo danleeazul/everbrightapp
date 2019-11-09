7cb4b56b804732742bd777316af3a62fbaa3f233
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-09 10:16:28 +0800

    v3

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,8 @@ $page_title="Everbright App";
  
 // include login checker
 // $require_login=true;
-// include_once "login_checker.php";
+include_once "login_checker.php";
+
 
 // include page header HTML
 include_once '../header.php';
