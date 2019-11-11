7d7719e172b964e33b58388e1d44cd9c4afc51b3
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-11 11:28:21 +0800

    v3

--- admin/login_checker.php
+++ admin/login_checker.php
@@ -4,7 +4,6 @@
 // if the session value is empty, he is not yet logged in, redirect him to login page
 if(empty($_SESSION['logged_in'])){
     header("Location: {$home_url}login.php?action=not_yet_logged_in");
-
 }
  
 // if access level was not 'Admin', redirect him to login page
