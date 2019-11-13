b4f32e6de1c543d081781401a4f342475f035cf6
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-13 08:40:18 +0800

    v1

--- login.php
+++ login.php
@@ -103,7 +103,7 @@ $access_denied=false;
 
                 <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
                 <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
                 <label for="inputPassword" class="sr-only">Password</label>
                 <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
                 <div class="checkbox mb-3">
