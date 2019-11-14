e60f52201dcb2c66bfb09593bb00ab011106b8f9
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-14 17:23:06 +0800

    v4

--- register_user.php
+++ register_user.php
@@ -245,7 +245,7 @@ if($_POST){
        </div> -->
             <hr class="mb-4">
             <div class="text-right">
-            <a href='/everbrightapp/admin/users.php'><button type="button" class="btn btn-outline-secondary">Cancel</button></a>
+            <!-- <a href='/everbrightapp/admin/users.php'><button type="button" class="btn btn-outline-secondary">Cancel</button></a> -->
             <button type="submit" value='Save'  class="btn btn-primary">Submit</button>
             <br />
             <br />
