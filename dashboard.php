35f698dba290e55690e70e08706837476f87c374
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-15 17:04:34 +0800

    v4

--- dashboard.php
+++ dashboard.php
@@ -7,7 +7,7 @@ $page_title="Everbright App";
  
 // include login checker
 $require_login=true;
-include_once "login_checker.php";
+include_once "login_checker_two.php";
 
  // to prevent undefined index notice
  $action = isset($_GET['action']) ? $_GET['action'] : "";
