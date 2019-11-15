80a878fa81bfea614d06aa3345ba869056be537d
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-15 17:06:52 +0800

    v5

--- login_checker_two.php
+++ login_checker_two.php
@@ -4,9 +4,9 @@
 // if $require_login was set and value is 'true'
 if(isset($require_login) && $require_login==true){
     // if user not yet logged in, redirect to login page
-    if(!isset($_SESSION['access_level'])){
+    
         header("Location: {$home_url}login.php?action=please_login");
-    }
+    
 }
 
  
