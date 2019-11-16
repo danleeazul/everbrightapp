a26cca0fea46e30407f7703fa981f4362726d55d
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-16 11:29:19 +0800

    v5

--- login_checker.php
+++ login_checker.php
@@ -9,7 +9,13 @@ if(isset($_SESSION['access_level']) && $_SESSION['access_level']=="Admin"){
 // if $require_login was set and value is 'true'
 else if(isset($require_login) && $require_login==true){
     // if user not yet logged in, redirect to login page
-    if(!isset($_SESSION['access_level'])){
+    if(isset($_SESSION['access_level']) && $_SESSION['access_level']!="Admin"){
+        header("Location: {$home_url}login.php?action=please_login");
+    }
+    elseif (isset($_SESSION['access_level']) && $_SESSION['access_level']!="Officer") {
+        header("Location: {$home_url}login.php?action=please_login");
+    }
+    elseif (isset($_SESSION['access_level']) && $_SESSION['access_level']!="Customer") {
         header("Location: {$home_url}login.php?action=please_login");
     }
 }
