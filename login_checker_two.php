49b5441ddb3525bdc93f312280e60d6280472820
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-16 11:04:22 +0800

    v2

--- login_checker_two.php
+++ login_checker_two.php
@@ -3,17 +3,17 @@
  
 // if $require_login was set and value is 'true'
     // if user not yet logged in, redirect to login page
-
+if(isset($require_login) && $require_login==true){
     if($_SESSION['access_level']!="Admin"){
-        header("Location: {$home_url}login.php?action=not_admin");
+        header("Location: {$home_url}login.php");
         if($_SESSION['access_level']!="Officer"){
-            header("Location: {$home_url}login.php?action=not_admin");
+            header("Location: {$home_url}login.php");
             if($_SESSION['access_level']!="Customer"){
-                header("Location: {$home_url}login.php?action=not_admin");
+                header("Location: {$home_url}login.php");
             }
         }
     }
-
+    }
 else{
     // no problem, stay on current page
 }
