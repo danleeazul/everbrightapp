d0268d776d799a119b67d872d8c1323b8b74bf57
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-16 11:36:07 +0800

    v7

--- login_checker.php
+++ login_checker.php
@@ -5,6 +5,10 @@
 if(isset($_SESSION['access_level']) && $_SESSION['access_level']=="Admin"){
     header("Location: {$home_url}admin/dashboard.php?action=logged_in_as_admin");
 }
+
+elseif (isset($_SESSION['access_level']) && $_SESSION['access_level']=="Officer") {
+    header("Location: {$home_url}admin/dashboard.php?action=logged_in_as_Officer");
+}
  
 // if $require_login was set and value is 'true'
 else if(isset($require_login) && $require_login==true){
