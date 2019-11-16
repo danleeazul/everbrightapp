02a7753683b277fb16575e187e79914e7195110e
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-16 09:42:25 +0800

    v7

--- login_checker.php
+++ login_checker.php
@@ -1,5 +1,12 @@
 <?php
 // login checker for 'customer' access level
+
+if(isset($require_login) && $require_login==true){
+    // if user not yet logged in, redirect to login page
+    if(!isset($_SESSION['access_level'])){
+        header("Location: {$home_url}login.php?action=please_login");
+    }
+}
  
 // if access level was not 'Admin', redirect him to login page
 if(isset($_SESSION['access_level']) && $_SESSION['access_level']=="Admin"){
@@ -11,12 +18,7 @@ elseif(isset($_SESSION['access_level']) && $_SESSION['access_level']=="Officer")
 }
  
 // if $require_login was set and value is 'true'
-else if(isset($require_login) && $require_login==true){
-    // if user not yet logged in, redirect to login page
-    if(!isset($_SESSION['access_level'])){
-        header("Location: {$home_url}login.php?action=please_login");
-    }
-}
+
  
 // if it was the 'login' or 'register' or 'sign up' page but the customer was already logged in
 else if(isset($page_title) && ($page_title=="Login" || $page_title=="Sign Up")){
