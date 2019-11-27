248440db4d8ec04158b0f446f927c1f26c51bd65
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-27 11:59:35 +0800

    v2

--- login.php
+++ login.php
@@ -118,8 +118,8 @@ $verified=false;
         ?>
 
                 <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <label for="inputEmail" class="sr-only">Username</label>
+                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
                 <label for="inputPassword" class="sr-only">Password</label>
                 <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
