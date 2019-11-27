b9be063e5508915cfb23eb60dc0e22e769e7a1d7
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-27 14:51:17 +0800

    v3

--- index.php
+++ index.php
@@ -140,6 +140,7 @@ $verified=false;
         ?>
 
                 <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <h6 class="h6 mb-3 font-weight-normal">You may now use your new login credentials</h6>
                 <label for="inputEmail" class="sr-only">Email address</label>
                 <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
                 <label for="inputPassword" class="sr-only">Password</label>
