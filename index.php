7f99a78614b59968fe51194c8879bd2f6f9164c9
danleeazul <32784595+danleeazul@users.noreply.github.com>
2019-11-28 09:22:46 +0800

    v1

--- index.php
+++ index.php
@@ -141,8 +141,8 @@ $verified=false;
 
                 <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
                 <h6 class="h6 mb-3 font-weight-normal">You may now use your new login credentials</h6>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <label for="inputEmail" class="sr-only">Username</label>
+                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
                 <label for="inputPassword" class="sr-only">Password</label>
                 <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
